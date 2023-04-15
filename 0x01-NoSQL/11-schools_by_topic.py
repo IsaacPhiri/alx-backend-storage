8ec88682ef99d8e126056b4f2eb80b60e210c90d
IsaacPhiri <isaacphiri315@gmail.com>
2023-04-15 16:45:00 +0000

    Added a Python function that returns the list of school having a specific topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+a function that list all topics that match
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Args:
+    mongo_collection - the collection
+    topic - topic to match
+    """
+
+    sch_list = list(mongo_collection.find({"topics:" topic}))
+    return sch_list
